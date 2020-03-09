08bff6a7bf38c3113338b80ad2771abb42dfb5a5
cawnj <57002528+cawnj@users.noreply.github.com>
2020-03-09 20:43:13 +0000

    tts bee movie script

--- speak.ino
+++ speak.ino
@@ -4,6 +4,7 @@ void setup() {
 }
 
 void loop() {
+  // open powershell
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.delay(100);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
@@ -11,10 +12,10 @@ void loop() {
   DigiKeyboard.print("powershell");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(1000);
-
+  // the command itself
   DigiKeyboard.print("Invoke-WebRequest -Uri \"https://raw.githubusercontent.com/cawnj/digispark-scripts/master/entire_bee_movie_script.txt\" -OutFile \"text.txt\"; $Contents = Get-Content \"text.txt\"; Add-Type -AssemblyName System.speech; $Narrator = New-Object System.Speech.Synthesis.SpeechSynthesizer; $Narrator.SelectVoice('Microsoft Zira Desktop'); $Narrator.Rate = 2; $Narrator.Speak($Contents)");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(100);
+  // Minimize powershell window
   DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_ALT_LEFT);
   DigiKeyboard.sendKeyStroke(KEY_N);
   
